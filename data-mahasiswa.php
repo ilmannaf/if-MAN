418bf675280668334ef1682bedeca668c96a4753
ilmannaf <ilmannafia2006@gmail.com>
2026-06-03 11:23:46 +0700

    retrive data

--- data-mahasiswa.php
+++ data-mahasiswa.php
@@ -1,11 +1,32 @@
+<?php
+
+$connection = mysqli_connect("localhost", "root", "root", "if_man");
+if ($connection) {
+    echo"koneksi berhasil";
+    $query = "SELECT * FROM mahasiswa";
+    $result = mysqli_query($connection, $query);
+
+    ///mysqli_fetch_arrya
+    ///mysqli_fetch_assoc
+    ///mysqli_fetch_object
+    ///mysqli_fetch_row
+
+    
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>data mahasiswa</title>
+    <link rel="stylesheet" href="asset/css/dataMahasiswa.css">
+    
 </head>
 <body>
+    
     <h1>INFORMATIKA</h1>
     <hr>
     <table border= "1" cellpadding="0" cellspacing="10px">
@@ -20,29 +41,39 @@
      <a href="inputdata.php">
         <button>Tambah Data</button>
      </a>
-   <table border= "1" cellspacing="5px">
-        <tr>
-            <th rowspan="2">no</th>
-            <th rowspan="2">Nama</th>
-            <th rowspan="2">Nim</th>
-            <th rowspan="2">foto</th> 
-            <th colspan="3">nilai</th>
-        </tr>
-        <tr>
-            <th>UTS</th>
-            <th>UAS</th>    
-            <th>TUGAS</th>
-        </tr>
-        <tr>
-            <td align="center">1</td>
-            <td>M. ilman  nafia</td>
-            <td>13182420025</td>
-            <td><img src="C:\ilman\SEMESTER 4\WEB\if-MAN\asset\images\ambarus.jpg" alt="Foto 1" width="70px"></td>
-            <td align="center">85</td>
-            <td align="center">90</td>
-            <td align="center">88</td>
-        </tr>
-    </table>
+   <table border="1" cellspacing="5px">
+    <tr>
+        <th>No</th>
+        <th>Nama</th>
+        <th>NIM</th>
+        <th>Foto</th>
+        <th>Jurusan</th>
+        <th>Email</th>
+        <th>No HP</th>
+        <th>Aksi</th>
+    </tr>
+ 
+    <?php 
+    $i = 1;
+    while($mhs = mysqli_fetch_assoc($result)) : 
+    ?>
+    <tr>
+        <td align="center"><?php echo $i++; ?></td>
+        <td align="center"><?= $mhs['nama']; ?></td>
+        <td align="center"><?= $mhs['nim']; ?></td>
+        <td align="center">
+            <img src="asset/images/ambarus.jpg" width="70">
+        </td>
+        <td align="center"><?= $mhs['jurusan']; ?></td>
+        <td align="center"><?= $mhs['email']; ?></td>
+        <td align="center"><?= $mhs['no_hp']; ?></td>
+        <td>
+        <a href="editdata.php?id=<?= $mhs['id']; ?>">Edit</a> |
+        <a href="deletedata.php?id=<?= $mhs['id']; ?>">Delete</a>
+        </td>
+    </tr>
+    <?php endwhile; ?>
+</table>
     <hr>
     <table border="1" cellpadding="5" cellspacing="0">
         <tr>
@@ -56,6 +87,7 @@
             <td>2,2</td>
             <td>2,3</td>
             <td>2,4</td>
+        </tr>
         <tr>
             <td>3,1</td>
             <td>3,2</td>
@@ -67,6 +99,7 @@
             <td>4,2</td>
             <td>4,3</td>
             <td>4,4</td>
+        </tr>
     </table>
 </body>
 </html>
